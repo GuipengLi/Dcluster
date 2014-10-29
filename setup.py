26b1f2c797d097ddc9bcb83cf6404879816fface
GuipengLi <guipeng.lee@gmail.com>
2014-10-29 10:36:23 +0800

    Init
    init

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name = 'sharepathway',
-    packages = ['sharepathway'], # this must be the same as the name above
-    version = '0.4.2',
-    description = 'A Python package for KEGG pathway enrichment analysis with multiple gene lists',
+    name = 'Dcluster',
+    packages = ['Dcluster'], # this must be the same as the name above
+    version = '0.1.0',
+    description = 'A Python package for Clustering by fast search and find of density peaks',
     author = 'Guipeng Li',
     author_email = 'guipenglee@gmail.com',
-    url = 'https://github.com/GuipengLi/sharepathway',   # use the URL to the github repo
-    keywords = ['detection', 'pathway', 'enrichment', 'share', 'multiple gene lists'], # arbitrary keywords
+    url = 'https://github.com/GuipengLi/Dcluster',   # use the URL to the github repo
+    keywords = ['Cluster', 'fast', 'density'], # arbitrary keywords
     classifiers = [],
     long_description=open('README.rst').read(),
     include_package_data=True,
-    install_requires=["numpy >= 1.4.0", "scipy >= 0.9.0"],
+    install_requires=["numpy >= 1.2.0"],
 )
